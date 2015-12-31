d4683b9e3cbdf02d8db23a756f49064fbd046a4e
djun1 <djun@live.ca>
2015-12-30 21:59:29 -0800

    First Commit
    Able to download, save, read, and display text from METAR file.

---
+++ App1/App1.Shared/XMLParserClass.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace App1
+{
+    class XMLParserClass
+    {
+        private XElement XMLDocElements;
+        
+        public XMLParserClass(string XMLDocumentName)
+        {
+            try
+            {
+                XMLDocElements = XElement.Load(XMLDocumentName);
+            }
+            catch(Exception e)
+            {
+                
+            }
+            
+        }
+
+        public void SetSourceFile(string FileName)
+        {
+            try
+            {
+                XMLDocElements = XElement.Load(FileName);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void ReadWx(string RegionName)
+        {
+            IEnumerable<XElement> LocationNode =
+                    from EnumLocation in XMLDocElements.Elements("location")
+                    select EnumLocation;
+
+            IEnumerable<XElement> Region =
+                    from EnumRegion in LocationNode.Descendants() 
+                    where (string)EnumRegion.Attribute("Region") == RegionName
+                    select EnumRegion;
+
+            //string RegionName = EnumRegion.Value;
+            
+        }
+
+        public void ReadCitiesInProvince(string ProvinceName, List<string> CityNameList)
+        {
+            if(XMLDocElements != null)
+            {
+                IEnumerable<XElement> ProvinceNode =
+                    from EnumProv in XMLDocElements.Elements("Province")
+                    where (string)EnumProv.Attribute("Name") == ProvinceName
+                    select EnumProv;
+
+                IEnumerable<XElement> Cities =
+                    from EnumCity in ProvinceNode.Descendants()
+                    select EnumCity;
+
+                foreach (XElement EnumCity in Cities)
+                {
+
+                    string CityName = EnumCity.Value;
+                    CityNameList.Add(CityName);
+                    CityNameList.Sort();
+                }
+            }
+            
+        }
+
+        //public void ReadCitiesInProduct(string ProductName, List<string> CityNameList)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Cities =
+        //            from EnumCity in ProductNode.Descendants()
+        //            select EnumCity;
+
+        //        foreach (XElement EnumCity in Cities)
+        //        {
+
+        //            string CityName = EnumCity.Value;
+        //            CityNameList.Add(CityName);
+        //            CityNameList.Sort();
+        //        }
+        //    }
+
+        //}
+
+        //public List<string> ReadProductList()
+        //{
+        //    List<string> ProductList = null;
+
+        //    if (XMLDocElements != null)
+        //    {
+        //        ProductList = new List<string>();
+
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProd in XMLDocElements.Elements("Product")
+        //            select EnumProd;
+
+        //        foreach (XElement Product in ProductNode)
+        //        {
+        //            string ProductName = Product.FirstAttribute.Value;
+        //            ProductList.Add(ProductName);
+        //        }
+
+        //        ProductList.Sort();
+        //    }
+
+        //    return ProductList; 
+        //}
+        //public void ReadProductOptionList(string ProductName, List<string> OptionList)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where EnumOptions.Name == "Option"
+        //            select EnumOptions;
+
+        //        foreach (XElement EnumOption in Options)
+        //        {
+
+        //            string Option = EnumOption.FirstAttribute.Value;
+        //            OptionList.Add(Option);
+        //        }
+
+        //        OptionList.Sort();
+        //    }
+
+        //}
+
+        //This is used to read the list of Option2 "choices"
+        //public void ReadChoiceList(string ProductName, string Option, List<string> ChoiceList)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where (string)EnumOptions.Attribute("Name") == Option
+        //            select EnumOptions;
+
+        //        IEnumerable<XElement> Choices =
+        //            from EnumChoice in Options.Descendants()
+        //            where EnumChoice.Name == "Option2"
+        //            select EnumChoice;
+
+        //        foreach (XElement EnumChoice in Choices)
+        //            ChoiceList.Add(EnumChoice.FirstAttribute.Value);
+
+        //        ChoiceList.Sort();
+        //    }
+
+        //}
+
+        //For use only with weather camera data
+        //public void ReadDirList(string ProductName, string RegionName, string StationName, List<string> DirList)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where (string)EnumOptions.Attribute("Name") == RegionName
+        //            select EnumOptions;
+
+        //        if (StationName != null)
+        //        {
+        //            IEnumerable<XElement> Choices =
+        //            from EnumChoice in Options.Descendants()
+        //            where (string)EnumChoice.Attribute("Name") == StationName
+        //            select EnumChoice;
+
+        //            IEnumerable<XElement> Dirs =
+        //            from EnumDir in Choices.Descendants()
+        //            where EnumDir.Name == "Dir"
+        //            select EnumDir;
+
+        //            IEnumerable<XElement> Code =
+        //            from EnumCode in Choices.Descendants()
+        //            where EnumCode.Name == "Code"
+        //            select EnumCode;
+
+        //            DirList.AddRange(Dirs.First().Value.Split(','));
+        //            DirList.Sort();
+        //        }
+        //    }
+        //}
+
+        //public List<string> ReadProvinceList()
+        //{
+        //    List<string> ProvinceList = new List<string>();
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> Provinces =
+        //            from EnumProv in XMLDocElements.Elements("Province")
+        //            select EnumProv;
+
+        //        IEnumerable<XAttribute> attList =
+        //            from at in Provinces.Attributes()
+        //            select at;
+
+        //        foreach (XAttribute EnumProv in attList)
+        //        {
+        //            ProvinceList.Add(EnumProv.Value);
+        //        }
+        //    }
+
+        //    return ProvinceList;
+        //}
+
+        //public string ReadBaseURL(string ProductName)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where EnumOptions.Name.ToString().Equals("BaseURL")
+        //            select EnumOptions;
+
+        //        return Options.First().Value.ToString();
+        //    }
+
+        //    return null;
+
+        //}
+
+        //public string ReadCode(string ProductName, string RegionName, string StationName)
+        //{
+        //    IEnumerable<XElement> Choices = null;
+
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where (string)EnumOptions.Attribute("Name") == RegionName
+        //            select EnumOptions;
+
+        //        if(StationName != null)
+        //        {
+        //            Choices =
+        //            from EnumChoice in Options.Descendants()
+        //            where (string)EnumChoice.Attribute("Name") == StationName
+        //            select EnumChoice;
+
+        //            IEnumerable<XElement> Codes =
+        //            from EnumOptions in Choices.Descendants()
+        //            where EnumOptions.Name.ToString().Equals("Code")
+        //            select EnumOptions;
+        //            if(Codes.Count() > 0)
+        //                return Codes.First().Value.ToString();
+        //        }
+        //        else
+        //            return Options.First().Value.ToString();
+        //    }
+
+        //    return "";
+
+        //}
+
+        //public string ReadNodeField(string ProductName, string Region, string Station, string Dir, string Field)
+        //{
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> ProductNode =
+        //            from EnumProv in XMLDocElements.Elements("Product")
+        //            where (string)EnumProv.Attribute("Name") == ProductName
+        //            select EnumProv;
+
+        //        IEnumerable<XElement> Options =
+        //            from EnumOptions in ProductNode.Descendants()
+        //            where EnumOptions.Name.ToString().Equals(Field)
+        //            select EnumOptions;
+
+        //        return Options.First().Value.ToString();
+        //    }
+
+        //    return "";
+
+        //}
+
+        //public string GetCityCode(string CityName)
+        //{
+        //    XElement City, CityCode;
+            
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> CityNode =
+        //            from EnumProv in XMLDocElements.Elements("City")
+        //            where (string)EnumProv.Attribute("Name") == CityName
+        //            select EnumProv;
+
+        //        City = CityNode.First();
+        //        CityCode = (XElement)City.FirstNode;
+                                                
+        //        return CityCode.Value.ToString();
+        //    }
+
+        //    return "Failed";
+        //}
+
+        //public string GetHomeURL(string CityName)
+        //{
+        //    XElement City, BaseURL;
+
+        //    if (XMLDocElements != null)
+        //    {
+        //        IEnumerable<XElement> CityNode =
+        //            from EnumProv in XMLDocElements.Elements("City")
+        //            where (string)EnumProv.Attribute("Name") == CityName
+        //            select EnumProv;
+
+        //        City = CityNode.First();
+        //        BaseURL = (XElement)City.LastNode;
+
+        //        return BaseURL.Value.ToString();
+        //    }
+
+        //    return "Failed";
+        //}
+
+
+    }
+}
---
+++ App1/App1.Windows/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using App1.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+//using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+
+
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace App1
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusText.Text = String.Empty;
+
+            await GetMETARUsingHTTP();
+
+            StorageFolder Folder = await ApplicationData.Current.TemporaryFolder.GetFolderAsync("Files");
+            StorageFile File = await Folder.GetFileAsync("METAR.txt");
+
+            var Text = await FileIO.ReadLinesAsync(File);
+
+            foreach (var line in Text.AsEnumerable().Skip(1))
+            {
+                StatusText.Text += line + "\r\n";
+            }
+
+            //Alternate method, but unsure how to handle max i and when lines < i
+            //IList<string> lines = await FileIO.ReadLinesAsync(file);
+
+            //for (int i = 1; i <= 5; i++ )
+            //{
+            //    StatusText.Text += lines[i];
+            //    if (lines == null) break; ??
+            //}
+        }
+
+        public static async Task GetMETARUsingHTTP()
+        {
+            Uri uri = new Uri("http://dd.weather.gc.ca/bulletins/alphanumeric/20151229/SA/CYVR/05/SACN62_CYVR_290500___36812");
+            HttpClient Client = new HttpClient();
+
+            var Message = await Client.GetAsync(uri);
+            var Content = await Message.Content.ReadAsStringAsync();
+
+            StorageFolder Folder = await ApplicationData.Current.TemporaryFolder.CreateFolderAsync("Files", CreationCollisionOption.OpenIfExists);
+            StorageFile File = await Folder.CreateFileAsync("METAR.txt", CreationCollisionOption.ReplaceExisting);
+
+            await FileIO.WriteTextAsync(File, Content);
+
+            Client.Dispose();
+        }
+    }
+}
